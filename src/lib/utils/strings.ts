c431d5fa1803ba0d6982aa585431c0308697c694
Marco Antônio <macsoares1@gmail.com>
2021-10-28 16:56:58 -0300

    Improved Auth Flow / Added Strings object

---
+++ src/lib/utils/strings.ts
@@ -0,0 +1,34 @@
+const Strings = {
+  anonymousUser: 'Usuário anônimo',
+  enterYourName: 'Digite seu nome',
+  enterYourRegister: 'Digite seu registro',
+  initialRegistration: 'Cadastro inicial',
+  loading: 'Carregando...',
+  license: 'Habilitação',
+  licenseNumber: 'Número da habilitação',
+  licenseExpirarion: 'Data de validade',
+  management: 'Gerência',
+  managementDropdownLabel: 'Selecionar gerência',
+  newChecklistDropdownLabel: 'Selecionar veículo',
+  next: 'Próximo',
+  openMenu: 'Abrir menu',
+  register: 'Registro',
+  saveChecklist: 'Salvar checklist',
+  screenTitles: {
+    authFlow: {
+      license: 'Cadastro inicial - Habilitação',
+      management: 'Cadastro inicial - Gerência',
+      name: 'Cadastro inicial - Nome',
+      register: 'Cadastro inicial - Registro'
+    },
+    checklists: 'Meus Checklists',
+    home: 'Home',
+    newChecklist: 'Novo Checklist',
+    viewChecklist: 'Visualizar Checklist'
+  },
+  selectAll: 'Selecionar todos',
+  undefined: 'Indefinido',
+  vehiclesChecklist: 'Checklist Veícular'
+}
+
+export default Strings
